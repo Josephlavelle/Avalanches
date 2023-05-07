7bac5c0ca2b8bfc40883e8766f40409b60cb093e
josephlavelle <josephlavelle99@gmail.com>
2023-05-07 00:58:50 -0400

    evaluate file now outputs response data. Added note with useful links

---
+++ Notes.md
@@ -0,0 +1,9 @@
+Useful Links :
+
+API DOCS:
+Weather Data:
+
+https://open-meteo.com/en/docs/historical-weather-api
+https://open-meteo.com/en/docs
+
+Elevation: https://developers.google.com/maps/documentation/elevation/overview
--- evaluate.py
+++ evaluate.py
@@ -1,9 +1,11 @@
 import requests as req
-latitude, longitude = 51.46, -116,16
+import pandas as pd
+latitude, longitude = 51.46, -116.16
 api = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m"
 response = req.get(f"{api}")
 if response.status_code == 200:
     print("successfully fetched data")
-    print(response.json())
+    df = pd.DataFrame.from_dict(response.json())
+    df.to_csv("testing/responseData.csv")
 else:
     print("Error")
