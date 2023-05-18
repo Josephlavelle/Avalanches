cb01c5e54ee9dd4eff636abc767f0a3aa7efb292
josephlavelle <josephlavelle99@gmail.com>
2023-05-17 20:31:55 -0400

    formatting fixes

--- evaluate.py
+++ evaluate.py
@@ -3,5 +3,5 @@ from modules.Forecaster import Forecaster
 print("Program Started")
 testForecast = Forecaster((51.1784,-115.5708),["openWeatherMap","tomorrow.io"])
 print("Forecaster Initialized")
-testForecast.importForecastData(apiKeySource="apiKeys.txt")
+testForecast.importForecastData(24,apiKeySource="apiKeys.txt")
 print(testForecast.apiForecasts)
--- modules/Forecaster.py
+++ modules/Forecaster.py
@@ -7,8 +7,8 @@ class Forecaster:
         self.apis = apis
         self.apiForecasts = pd.DataFrame(columns=["Time", "Temperature"])
     def importForecastData(self, 
-                           timeframeHours: int = 24, 
-                           apiKeySource: str = "apiKeys.txt"):
+                           timeframeHours: int, 
+                           apiKeySource: str):
         latitude, longitude = self.lat, self.longi
         apiKeys = getAuth(apiKeySource)
         forecastData = []
@@ -17,14 +17,13 @@ class Forecaster:
             print(f"Attemping to GET {api} data")
             url = ""
             if api == "openWeatherMap":
-                url = f"http://api.openweathermap.org/data/2.5/forecast?lat={latitude}&lon={longitude}&appid={apiKey}"
+                url = f"http://api.openweathermap.org/data/2.5/forecast?lat={latitude}&lon={longitude}&appid={apiKey}&units=metric"
             elif api == "tomorrow.io":
                 url = f"https://api.tomorrow.io/v4/timelines?location={latitude},{longitude}&fields=temperature&timesteps=1h&units=metric&apikey={apiKey}"
-            elif api == "weatherStack":
-                url = f"http://api.weatherstack.com/forecast?access_key={apiKey}&query={latitude},{longitude}"
             else:
                 print(f"{api} not configured")
                 continue
+            # Make API call
             response = req.get(url)
             if response.status_code == 200:
                 forecastData.append(response.json())
@@ -41,15 +40,11 @@ class Forecaster:
                 if "list" in data:
                     # openWeatherMap
                     temperature = data["list"][hour]["main"]["temp"]
-                    forecast["openWeatherMap"] = round((temperature - 273.15),2)
+                    forecast["openWeatherMap"] = temperature
                 elif "data" in data and "timelines" in data["data"]:
                     # tomorrow.io
                     temperature = data["data"]["timelines"][0]["intervals"][hour]["values"]["temperature"]
                     forecast["tomorrow.io"] = temperature
-                elif "current" in data and "forecast" in data:
-                    # WeatherStack
-                    temperature = data["forecast"][hour]["temperature"]
-                    forecast["WeatherStack"] = temperature
 
             # Add the forecast as a row to the DataFrame
             df = df._append({"Time": hour + 1, "Temperature": forecast}, ignore_index=True)
